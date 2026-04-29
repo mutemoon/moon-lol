ee825bfe4070c77cacf2cdc896d19fe312539a20
Haoran Zhao <1036066414@qq.com>
2026-04-29 23:27:24 +0800

    Update extract.rs

--- examples/extract.rs
+++ examples/extract.rs
@@ -4,7 +4,7 @@ use league_to_lol::extract::extract_all;
 
 fn main() {
     let game_path = r"D:\WeGameApps\英雄联盟\Game";
-    // extract_all(game_path);
+    extract_all(game_path);
     post_process_mapgeo();
 }
 
