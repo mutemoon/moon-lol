484d87e47eb41944b3c9891b9d37d8242d78ccc8
Haoran Zhao <1036066414@qq.com>
2026-05-06 16:02:45 +0800

    refactor: 清理冗余 example 并更新 headless 加载逻辑

--- examples/headless.rs
+++ examples/headless.rs
@@ -1,23 +1,18 @@
 use bevy::prelude::*;
+use bevy::winit::WinitPlugin;
 use lol_champions::PluginChampions;
 use lol_core::PluginCore;
 use lol_core::game::GameScenes;
+use lol_render::PluginRender;
 
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins.build().set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "classic 1v1 fiora".to_string(),
-                    resolution: (300, 300).into(),
-                    position: WindowPosition::At((0, 1000).into()),
-                    ..default()
-                }),
-                ..default()
-            }),
-            PluginChampions,
+            DefaultPlugins.build().disable::<WinitPlugin>(),
             PluginCore,
+            PluginRender,
+            PluginChampions,
         ))
-        .insert_resource(GameScenes::new(vec!["games/attack.ron".to_owned()]))
+        .insert_resource(GameScenes::new(vec!["games/test.ron".to_owned()]))
         .run();
 }
